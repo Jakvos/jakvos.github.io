2d45b8cb81fddc8f0575837e92616da5dfdbd9dd
Jakvos <87606951+Jakvos@users.noreply.github.com>
2024-10-10 19:32:57 +0200

    Halloween

---
+++ script.js
@@ -0,0 +1,6 @@
+function zobrazStracháka() {
+    const obrazky = ["strasidlo1.jpg", "strasidlo2.jpg", "strasidlo3.jpg"];
+    const nahodnyIndex = Math.floor(Math.random() * obrazky.length);
+    document.getElementById("strachak").src = obrazky[nahodnyIndex];
+    document.getElementById("strachak").style.display = "block";
+}
