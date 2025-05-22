7c4bc63e57b59eed4b6ac1a903428bf718fe35a8
Jakvos <87606951+Jakvos@users.noreply.github.com>
2025-05-22 19:19:45 +0200

    1.0.0.0

--- inf/welcome.js
+++ inf/welcome.js
@@ -1,13 +1,13 @@
 document.getElementById('start-game-btn').addEventListener('click', function() {
     // Přesměruje na stránku s hrou (předpokládáme, že soubor s hrou se jmenuje game.html)
     // Pokud je vaše hra stále v index.html, můžete místo přesměrování jen skrýt úvodní div a zobrazit herní div.
-    window.location.href = 'hadej-cislo/index.html'; // Změňte na název souboru vaší hry
+    window.location.href = 'hadej-cislo/hra.html'; // Změňte na název souboru vaší hry
 });
 
 document.getElementById('start-game-btn-2').addEventListener('click', function() {
     // Přesměruje na stránku s hrou (předpokládáme, že soubor s hrou se jmenuje game.html)
     // Pokud je vaše hra stále v index.html, můžete místo přesměrování jen skrýt úvodní div a zobrazit herní div.
-    window.location.href = 'umis-anglicky/umisanglicky.html'; // Změňte na název souboru vaší hry
+    window.location.href = 'umis-anglicky/index.html'; // Změňte na název souboru vaší hry
 });
 
 document.getElementById('how-to-play-btn').addEventListener('click', function() {
