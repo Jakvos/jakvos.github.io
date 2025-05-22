16fd6855148872b0b597b59efddf07bd70349561
Jakvos <87606951+Jakvos@users.noreply.github.com>
2025-05-22 11:33:33 +0200

    0.8.8.HT

--- inf/welcome.js
+++ inf/welcome.js
@@ -7,7 +7,7 @@ document.getElementById('start-game-btn').addEventListener('click', function() {
 document.getElementById('start-game-btn-2').addEventListener('click', function() {
     // Přesměruje na stránku s hrou (předpokládáme, že soubor s hrou se jmenuje game.html)
     // Pokud je vaše hra stále v index.html, můžete místo přesměrování jen skrýt úvodní div a zobrazit herní div.
-    window.location.href = '404.html'; // Změňte na název souboru vaší hry
+    window.location.href = 'umis-anglicky/umisanglicky.html.html'; // Změňte na název souboru vaší hry
 });
 
 document.getElementById('how-to-play-btn').addEventListener('click', function() {
